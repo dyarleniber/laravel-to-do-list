d97a56b8163de5ec3ae5ffda4212d9dc155fef8d
Dyarlen Iber <dyarlen1@gmail.com>
2020-08-12 14:21:20 -0300

    Routes, Controller, Model, Migration and Views for the tasks resource

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,48 +4,23 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel to-do list</title>
 
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
-                height: 100vh;
                 margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
             }
 
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
             }
 
             .links > a {
@@ -57,14 +32,10 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -80,20 +51,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @yield('content')
             </div>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('tasks', 'TaskController@index')
+    ->name('tasks.index');
+
+Route::get('tasks/create', 'TaskController@create')
+    ->name('tasks.create');
+
+Route::post('tasks', 'TaskController@store')
+    ->name('tasks.store');
+
+Route::get('tasks/{id}', 'TaskController@show')
+    ->name('tasks.show');
+
+Route::get('tasks/{id}/edit', 'TaskController@edit')
+    ->name('tasks.edit');
+
+Route::put('tasks/{id}', 'TaskController@update')
+    ->name('tasks.update');
+
+Route::delete('tasks/{id}', 'TaskController@destroy')
+    ->name('tasks.destroy');
+
+Route::redirect('/', 'tasks');
